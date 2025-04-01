23d97c80d4d461ee4be2e89e7b4a6ebf7a0b8334
rozalinaaag <rosalina2000@mail.ru>
2025-04-01 17:15:46 -0400

    prettier

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import { Home } from "../pages/home";
 
 function App() {
-
   return (
     <>
-
+      <Home />
+      <div>vfj</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
